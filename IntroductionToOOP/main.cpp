064ead1fab0ebaf086aadd758eaac6d5cf2eb505
ArtHoly26 <112950470+ArtHoly26@users.noreply.github.com>
2023-04-21 21:48:48 +0200

    Update

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,245 +1,363 @@
-//IntroductionToOOP
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-#define delimiter "\n--------------------------------------------\n"
-
-/*
----------------
-Создавая структуру или класс, мы создаем новый тип данных,
-пользовательский тип данных.
-				  класс - это тип данных
-				структуры - это тип данных
----------------
-*/
-
-class Point
+class Fraction
 {
-	double x;
-	double y;
 public:
-	double get_x()const
-	{
-		return x;
-	}
-	double get_y()const
-	{
-		return y;
-	}
-	void set_x(double x)
-	{
-		this->x = x;
-	}
-	void set_y(double y)
-	{
-		this->y = y;
-	}
 
-	//				Constructors:
-	Point()
-	{
-		x = y = 0;
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	Point(double x)
-	{
-		this->x = x;
-		this->y = 0;
-		cout << "1ArgConstructor:\t" << this << endl;
-	}
-	Point(double x, double y)
-	{
-		this->x = x;
-		this->y = y;
-		cout << "Constructor:\t\t" << this << endl;
-	}
-	Point(const Point& other)
-	{
-		this->x = other.x;
-		this->y = other.y;
-		cout << "CopyConstructor:\t" << this << endl;
-	}
-	~Point()
-	{
-		cout << "Destructor:\t\t" << this << endl;
-	}
+	Fraction();
+	Fraction(int integ);
+	Fraction(int den, int numer);
+	Fraction(int integ, int den, int numer);
+	Fraction(const Fraction& other);
+	void GetIntDenNum() const;
+	void SetIntDenNum(int integ,int den,int num);
+	Fraction& Transformation();
+	Fraction& operator =(const Fraction& other);
+	Fraction operator +(const Fraction& other);
+	Fraction operator -(const Fraction& other);
+	Fraction operator /(const Fraction& other);
+	Fraction operator *(const Fraction& other);
 
-	//				Operators:
-	Point& operator=(const Point& other)
-	{
-		this->x = other.x;
-		this->y = other.y;
-		cout << "CopyAssignment:\t\t" << this << endl;
-		return *this;
-	}
-
-	//				Methods:
-	double distance(const Point& other)const//Point other - другой объект (другая точка)
-	{
-		double x_distance = this->x - other.x;
-		double y_distance = this->y - other.y;
-		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
-		return distance;
-	}
-
-	void print()const
-	{
-		cout << "X = " << x << "\tY = " << y << endl;
-	}
+private:
+	int numer;
+	int den;
+	int integ;
 };
 
-double distance(const Point& A, const Point& B)
+int main()
 {
-	double x_distance = A.get_x() - B.get_x();
-	double y_distance = A.get_y() - B.get_y();
-	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
-	return distance;
+	setlocale(LC_ALL, "Rus");
+	Fraction fr1(0, 1, 5);  //Умножение
+	Fraction fr2(0, 5, 1);
+
+	Fraction fr3(4, 0, 0);
+	Fraction fr4(0, 3, 4);
+
+	Fraction fr5(0, 21, 30);
+	Fraction fr6(0, 5 , 15);
+
+	Fraction fr7(2, 0, 0);   //Деление
+	Fraction fr8(0, 1, 2);
+
+	Fraction fr9(0, 1, 2);
+	Fraction fr10(0, 1, 4);
+
+	Fraction fr11(0, 5, 7);
+	Fraction fr12(0, 3, 5);
+
+	Fraction fr13(0, 2, 3);  //Сложение
+	Fraction fr14(0, 1, 2);
+
+	Fraction fr15(0, 5, 1);
+	Fraction fr16(0, 1 ,5);
+
+	Fraction fr17(0, 2, 3);
+	Fraction fr18(0, 1, 2);
+
+	Fraction fr19(0, 3, 4);  //Вычитание
+	Fraction fr20(0, 1, 4);
+
+	Fraction fr21(0, 13, 7);
+	Fraction fr22(0, 3, 7);
+
+	Fraction fr23(0, 2, 3);
+	Fraction fr24(0, 1, 4);
+
+	Fraction fr01 = fr1 * fr2;
+	Fraction fr02 = fr3 * fr4;
+	Fraction fr03 = fr5 * fr6;
+
+	Fraction fr04 = fr7 / fr8;
+	Fraction fr05 = fr9 / fr10;
+	Fraction fr06 = fr11 / fr12;
+
+	Fraction fr07 = fr13 + fr14;
+	Fraction fr08 = fr15 + fr16;
+	Fraction fr09 = fr17 + fr18;
+
+	Fraction fr010 = fr19 - fr20;
+	Fraction fr011 = fr21 - fr22;
+	Fraction fr012 = fr23 - fr24;
+	
+	fr07.GetIntDenNum();
+	fr07.Transformation();
+	fr07.GetIntDenNum();
+	cout << "------------" << endl;
+	fr08.GetIntDenNum();
+	fr08.Transformation();
+	fr08.GetIntDenNum();
+	cout << "------------" << endl;
+	fr09.GetIntDenNum();
+	fr09.Transformation();
+	fr09.GetIntDenNum();
+	cout << "------------" << endl;
+	
 }
 
-//#define STRUCT_POINT
-//#define CONSTRUCTORS_CHECK
-//#define DISTANCE_CHECK
-//#define ASSIGNMENT_CHECK
-
-void main()
+Fraction::Fraction()
 {
-	setlocale(LC_ALL, "");
-
-#ifdef STRUCT_POINT
-	//type name;
-	int a;		//Объявление переменной 'a' типа 'int'
-	Point A;	//Объявление переменной 'A' типа 'Point'
-	//Объявление объекта 'A' структуры 'Point'
-	//Создание экземпляра 'A' структуры 'Point'
-	A.x = 2;
-	A.y = 3;
-	cout << A.x << "\t" << A.y << endl;
-	cout << sizeof(A) << endl;
-
-	Point* pA = &A;
-	cout << pA->x << "\t" << pA->y << endl;
-#endif // STRUCT_POINT
-
-#ifdef CONSTRUCTORS_CHECK
-	Point A;	//Default constructor
-	//A.set_x(2);
-	//A.set_y(3);
-	//cout << A.get_x() << "\t" << A.get_y() << endl;
-	A.print();
-
-	Point B = 5;	//Single-argument constructor
-	B.print();
-
-	Point C(2, 3);
-	C.print();
-
-	Point D = C;	//Copy constructor (Deep copy) new
-	//Shallow copy
-	D.print();
-
-	Point E;
-	E = D;		//Copy assignment (оператор присваивания)
-#endif // CONSTRUCTORS_CHECK
-
-#ifdef DISTANCE_CHECK
-	Point A(2, 3);
-	A.print();
-
-	Point B(7, 8);
-	B.print();
-	cout << delimiter << endl;
-	cout << "Расстояние от точки A до точки B: " << A.distance(B) << endl;
-	cout << delimiter << endl;
-	cout << "Расстояние от точки A до точки B: " << B.distance(A) << endl;
-	cout << delimiter << endl;
-	cout << "Расстояние между точками A и B:   " << distance(A, B) << endl;
-	cout << delimiter << endl;
-	cout << "Расстояние между точками B и A:   " << distance(B, A) << endl;
-	cout << delimiter << endl;
-#endif // DISTANCE_CHECK
-
-#ifdef ASSIGNMENT_CHECK
-	int a, b, c;
-	a = b = c = 0;
-	cout << a << "\t" << b << "\t" << c << endl;
-
-	Point A, B, C;
-	cout << delimiter << endl;
-	A = B = C = Point(2, 3);
-	cout << delimiter << endl;
-	A.print();
-	B.print();
-	C.print();
-#endif // ASSIGNMENT_CHECK
-
+	numer = den = integ = 0;
 }
 
-/*
-------------------------------------
-.  - Оператор прямого доступа (Point operator)
--> - Оператор косвенного доступа (Arrow operator)
-------------------------------------
-*/
+Fraction::Fraction(int integ)
+{
+	numer = den = 0;
+	this->integ = integ;
+}
 
-/*
-------------------------------------
-1. Encapsulation - это сокрытие определенной части класса от внешнего мира;
-	Модификаторы доступа:
-		private:	закрытые поля, доступны только внутри класса
-		public:		открытые поля, доступны из любого места программы
-		protected:	защищенные поля, доступны внутри нашего класса, и его дочерних классов
-	get/set-методы
-	get (взять, получить) позволяют получить значение закрытой переменной, т.е.,
-		открывают доступ к закрытым переменным на чтение.
-	set (задать, установить) позволяют задавать значения закрытой переменной в классе, т.е.,
-		открывают доступ к закрытым переменным на запись.
-2. Inheritance;
-3. Polymorphism;
-------------------------------------
-*/
+Fraction::Fraction(int den, int numer)
+{
+	integ = 0;
+	this->den = den;
+	this->numer = numer;
+	
+}
 
-/*
-------------------------------------
-1. Constructor - это метод, который создает объект, а именно, выделяет память под объект,
-   и инициализирует его поля при создании;
-2. ~Destructor - это метод, который уничтожает объект по завршении его времени жизни.
-   Время жизни объекта завршается при выходе за пределы области видимости, в которой он объявлен;
-3. Assignment operator;
-				Типы конструкторов:
-	-с параметрами;
-	-без параметров;
-	-по умолчанию - это конструктор, который может быть вызван без параметров;
-	-конструктор копирования;
-	-конструктор переноса;
-------------------------------------
-*/
+Fraction::Fraction(int integ, int den, int numer)
+{
+	this->integ = integ;
+	this->den = den;
+	this->numer = numer;
+}
 
-/*
-------------------------------------
-				Overloading rules:
-1. Перегрузить можно только существующие операторы.
-   Создавать новые операторы невозможно.
-	+  перегружается;
-	++ перегружается;
-	%  перегружается;
-	%% НЕ перегружается;
-	@  НЕ перегружфется;
-2. НЕ все существующие операторы можно перегрузить.
-   НЕ перегружаеются:
-	?: - Conditional ternary operator
-	:: - Scope operator (Оператор разрешения видимости)
-	.  - Point operator (Оператор прямого доступа)
-	.* - Pointer to member selection
-	#
-	##
-3. Перегруженные операторы сохраняют приоритет;
-4. Изменить поведение операторов над встроенными типами невозможно;
-------------------------------------
-	type operator@(parameters)
+Fraction::Fraction(const Fraction & other)
+{
+	this->integ = other.integ;
+	this->den = other.den;
+	this->numer = other.numer;
+}
+
+void Fraction::GetIntDenNum() const
+{
+	cout << "Целое число равно: " <<this->integ << endl;
+	cout << "Знаменатель равен: " <<this->den << endl;
+	cout << "Числитель равен  : " <<this->numer << endl;
+}
+
+void Fraction::SetIntDenNum(int integ,int den,int numer)
+{
+	this->integ = integ;
+	this->den = den;
+	this->numer = numer;
+}
+
+Fraction& Fraction:: Transformation()
+{
+	if (this->den == this->numer)
 	{
-		...;
-		group-of-statements;
-		...;
+			this->integ++;
+			this->den = 0;
+			this->numer = 0;
 	}
-------------------------------------
-*/
+
+	if (this->integ == 0)
+	{
+		if (this->den > this->numer)
+		{
+
+			if (this->numer != 1)
+			{
+				this->integ = (this->den / this->numer) + this->integ;
+				this->den = this->den - this->numer;
+				this->numer = this->numer;
+
+				if (this->den > this->numer)
+				{
+					this->den = this->den - this->numer;
+					this->numer = this->numer;
+				}
+			}
+
+			if (this->numer == 1)
+			{
+				this->integ = this->den / this->numer;
+				this->den = 0;
+				this->numer = 0;
+			}
+
+			if (this->den == this->numer)
+			{
+				this->den = 0;
+				this->numer = 0;
+			}
+		}
+	}
+	return *this;
+}
+
+Fraction& Fraction :: operator =(const Fraction& other)
+{
+	this->integ = other.integ;
+	this->den = other.den;
+	this->numer = other.numer;
+	return *this;
+}
+
+Fraction Fraction :: operator +(const Fraction& other)
+{
+	Fraction temp;
+	if (this->integ != 0 || other.integ != 0)        // Cмешанные числа
+	{
+		if (this->numer == other.numer)
+		{
+			temp.integ = this->integ + other.integ;
+			temp.den = this->den + other.den;
+			temp.numer = this->numer;
+		}
+		if (this->den == 0 || this->numer == 0)     //сложение целых чисел и правильной дроби
+		{
+			temp.integ = this->integ;
+			temp.den = other.den;
+			temp.numer = other.numer;
+		}
+
+		if (this->den == 0 && this->numer == 0)    //cложение целого и смешанного числа
+		{
+			temp.integ = this->integ + other.integ;
+			temp.den = other.den;
+			temp.numer = other.numer;
+		}
+
+		if (this->integ != 0 && other.integ != 0)
+		{
+			temp.integ = this->integ + other.integ; //сложение смешанных чисел
+			temp.den = ((this->den * other.numer) + (this->numer * other.den));
+			temp.numer = (this->numer * other.numer);
+		}
+	}
+
+
+	if (this->integ == 0 && other.integ == 0)         //Действия с дробями
+	{
+		if (this->numer == other.numer)               // Сложение дробей с одинаковыми знаменателями
+		{
+			temp.den = this->den + other.den;
+			temp.numer = this->numer;
+		}
+
+		if (this->numer!=other.numer)                  // Сложение дробей с разными знаменателями
+		{
+			temp.den = ((this->den * other.numer) + (this->numer * other.den));
+		    temp.numer = (this->numer * other.numer);
+			
+		}
+
+		return temp;
+	}
+}
+
+Fraction Fraction :: operator -(const Fraction& other)
+{
+	Fraction temp;
+	if (this->integ != 0 || other.integ != 0)        // Cмешанные числа
+	{
+		if (this->numer == other.numer)
+		{
+			temp.integ = this->integ - other.integ;
+			temp.den = this->den - other.den;
+			temp.numer = this->numer;
+		}
+		if (this->den == 0 || this->numer == 0)       //вычитание целых чисел и правильной дроби
+		{
+			temp.integ = this->integ;
+			temp.den = other.den;
+			temp.numer = other.numer;
+		}
+
+		if (this->den == 0 && this->numer == 0)       //вычитание целого и смешанного числа
+		{
+			temp.integ = this->integ + other.integ;
+			temp.den = other.den;
+			temp.numer = other.numer;
+		}
+
+		if (this->integ != 0 && other.integ != 0)
+		{
+			temp.integ = this->integ - other.integ;   //вычитание смешанных чисел
+			temp.den = ((this->den * other.numer) - (this->numer * other.den));
+			temp.numer = (this->numer * other.numer);
+		}
+	}
+
+
+	if (this->integ == 0 && other.integ == 0)         //Действия с дробями
+	{
+		if (this->numer == other.numer)               // Вычитание дробей с одинаковыми знаменателями
+		{
+			temp.den = this->den - other.den;
+			temp.numer = this->numer;
+		}
+
+		if (this->numer != other.numer)               // Вычиатние дробей с разными знаменателями
+		{
+			if (other.numer % this->numer)
+			{
+				temp.den = ((this->den * other.numer) - (this->numer * other.den));
+				temp.numer = (this->numer * other.numer);
+			}
+
+			if (other.numer % this->numer)
+			{
+				temp.den = ((this->den * other.numer) - (this->numer * other.den));
+				temp.numer = (this->numer * other.numer);
+			}
+
+			if (this->numer % other.numer)
+			{
+				temp.den = ((this->den * other.numer) - (this->numer * other.den));
+				temp.numer = (this->numer * other.numer);
+			}
+		}
+	}
+	return temp;
+}
+
+Fraction Fraction :: operator /(const Fraction& other)
+{
+	Fraction temp;
+	if (this->integ != 0 || other.integ != 0)        //Cмешанные числа
+	{
+		if (this->integ != 0 && other.integ == 0)    //Умножение дроби на число
+		{
+			temp.den = this->integ * other.numer;
+			temp.numer = other.den;
+		}
+	}
+
+
+	if (this->integ == 0 && other.integ == 0)
+	{
+		temp.den = this->den * other.numer;
+		temp.numer = this->numer * other.den;
+	}
+	return temp;
+}
+
+Fraction Fraction :: operator *(const Fraction& other)
+{
+	Fraction temp;
+	if (this->integ != 0 || other.integ != 0)        //Cмешанные числа
+	{
+		if (this->integ == 0 && other.integ != 0)    //Умножение дроби на число
+		{
+			temp.den = this->den * other.integ;
+			temp.numer = this->numer;
+		}
+
+		if (this->integ != 0 && other.integ == 0)    //Умножение дроби на число
+		{
+			temp.den = this->integ * other.den;
+			temp.numer = other.numer;
+		}
+	}
+
+
+	if (this->integ == 0 && other.integ == 0)
+	{
+		temp.den = this->den * other.den;
+		temp.numer = this->numer * other.numer;
+	}
+	return temp;
+}
