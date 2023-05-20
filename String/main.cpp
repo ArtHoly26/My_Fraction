c1145fd866384d2c46d0bbe80ca5b653b2e76458
ArtHoly26 <112950470+ArtHoly26@users.noreply.github.com>
2023-05-20 12:25:00 +0200

    Update

--- String/main.cpp
+++ String/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
 using namespace std;
+
+class String;
+String operator + (const String left, const String right);
 
 class String
 {
@@ -26,25 +28,36 @@ public:
 		for (int i = 0; i < size; i++) this->str[i] = other.str[i];
 		this->str[size] = '\0';
 	}
+	String(String&& other)
+	{
+		this->str = other.str;
+		other.str = nullptr;
+	}
 	~String()
 	{
 		delete[] this->str;
 	}
+
 	char* Get_Str()const
 	{
 		return str;
 	}
+
 	String& operator = (const String& other)
 	{
 		if (this -> str != nullptr) delete[] str;
-
 		int size = strlen(other.str);
 		this->str = new char[size + 1];
 		for (int i = 0; i < size; i++) this->str[i] = other.str[i];
 		this->str[size] = '\0';
-
 		return *this;
 	}
+	String& operator +=(const String& other)
+	{
+		return *this = *this + other;
+	}
+	char& operator [](int i) {return str[i];}
+	char& operator [](int i) const{return str[i];}
 };
 
 String operator + (const String left ,const String right)
@@ -53,22 +66,13 @@ String operator + (const String left ,const String right)
 	int size1 = strlen(left.Get_Str());
 	int size2 = strlen(right.Get_Str());
 	newStr = new char[size1 + size2 + 1];
-
-	int i = 0;
-	for (; i < size1; i++)
-	{
-		newStr.Get_Str()[i] = left.Get_Str()[i];
-	}
-
-	for (int j = 0; j < size2; j++, i++)
-	{
-		newStr.Get_Str()[i] = right.Get_Str()[j];
-	}
-	newStr.Get_Str()[size1 + size2] = '\0';
-
+	for (int i = 0; i < size1; i++) newStr[i] = left[i];
+	for (int i = 0; i < size2; i++) newStr[i+size1] = right[i];
+	
+	newStr[size1 + size2] = '\0';
 	return newStr;
 }
-std::ostream& operator<<(std::ostream& os, const String &obj)
+std::ostream& operator << (std::ostream& os, const String &obj)
 {
 	os << obj.Get_Str();
 	return os;
@@ -77,10 +81,11 @@ std::ostream& operator<<(std::ostream& os, const String &obj)
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	String str1= "Hello";
+	String str1= "Hellooooo";
 	cout << str1 << endl;
-	String str2 = "World";
+	String str2 = "Worldddddd";
 	cout << str2 << endl;
 	String str3 = str1 + str2;
 	cout << str3 << endl;
+	str1 += str2;
 }
